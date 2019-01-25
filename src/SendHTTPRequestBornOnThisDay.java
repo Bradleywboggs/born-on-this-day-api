1dcdbdb66d8f1ccd895b1bf52d0adcf6c52aa2ab
Bradley Boggs <bradleywboggs@gmail.com>
2019-01-24 22:23:35 -0600

    initial commit

---
+++ src/SendHTTPRequestBornOnThisDay.java
@@ -0,0 +1,23 @@
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class SendHTTPRequestBornOnThisDay {
+    public static void main(String[] args) throws IOException, JSONException{
+        System.out.println(onThisDay("12","25"));
+    }
+
+   
+}
+
+
+
+
+
+
