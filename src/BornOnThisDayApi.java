4416da3ae5118da64b792f93b07d2b5af90e2ecb
Bradley Boggs <bradleywboggs@gmail.com>
2019-01-26 21:33:06 -0600

    Minor edits Changed param type from String to int in getResponse(), as it is formatted to stString in the method already
    Edited javaDoc for parseAndFilterResponse() to reflect the change of name
    
    Added comments
    
    Adjusted spacing

--- src/BornOnThisDayApi.java
+++ src/BornOnThisDayApi.java
@@ -15,30 +15,35 @@ public class BornOnThisDayApi {
 
     /**
      * BornOnThisDay.getResponse()
-     * Sends HTTP request for 'births' to Wikimedia's 'On this day' api. The response is a JSON object,
+     * Sends HTTP request for 'births' to Wikimedia's 'On this day' api. The response is a JSON object
      * with a value of a JSON Array representing people born on the given day.
      * The response is read in and converted to a String
      *
-     * @param mm a two-character String representing the two digit month given by the user to be appended
+     * @param mm a two-digit integer representing the two-digit month given by the user to be parsed as a String and appended
      *           to the url path.
-     * @param dd a two character String representing the two digit day given by the user to be appended
-     *           to the url path
+     *
+     * @param dd a two-digit integer representing the two-digit day given by the user to be parsed as a String and appended
+     *           to the url path.
+     *
      * @return response.toString()
+     *
      * @author Bradley Boggs, bradleywboggs@gmail.com
      */
 
-    public static String getResponse(String mm, String dd) {
+    public static String getResponse(int mm, int dd) {
+
         try{
+
             String url = String.format("https://en.wikipedia.org/api/rest_v1/feed/onthisday/births/%s/%s", mm, dd);
-            //Instantiated URL object
+            // Instantiate URL object
             URL obj = new URL(url);
 
-           // Open Connection
+            // Open Connection
             HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
 
             con.setRequestMethod("GET");
 
-            //add request header
+            // Add request header
             con.setRequestProperty("Api-User-Agent", "sean.zicari@gmail.com");
             int responseCode = con.getResponseCode();
 
@@ -64,15 +69,16 @@ public class BornOnThisDayApi {
 
         } catch (MalformedURLException | ProtocolException | IOException e) {
             e.printStackTrace();
+
             return String.format("Exception: %s", e.toString());
         }
 
 }
     /**
-     * BornOnThisDay.convertAndFilterResponse() parses and filters the return value of getResponse()
+     * BornOnThisDay.parseAndFilterResponse() parses and filters the return value of getResponse()
      * to produce a new JSON Array to be used to render the front end.
      *
-     * @param response the return value of getResponse()
+     * @param response - the return value of getResponse()
      *
      * @return JSONArray of objects with the following key-value pairs:
      * "name"-name of the person, "year"-the year of their birth, "tagline"--a phrase describing the person,
@@ -82,15 +88,25 @@ public class BornOnThisDayApi {
      */
 
     public static JSONArray parseAndFilterResponse(String response) {
+
         try {
+
+            // Parse response string as JSONObject
             JSONObject initJSONObject = new JSONObject(response);
             JSONArray initArray = (JSONArray) initJSONObject.get("births");
+
+
+            // Instantiate a JSONArray for the return value
             JSONArray returnArray = new JSONArray();
 
             for (int i = 0; i < initArray.length(); i++) {
+
+                //Instantiate a JSONObject as an accumulator
                 JSONObject returnArrayElement = new JSONObject();
+
                 JSONObject arrayElement = (JSONObject) initArray.opt(i);
 
+                // Iterate through the JSON Objects checking for a desired key, and if present, store the value with a new key in the accumulator
                 if (arrayElement.has("year")) {
                     Integer year = (Integer) arrayElement.opt("year");
                     returnArrayElement.put("year", year);
@@ -124,16 +140,19 @@ public class BornOnThisDayApi {
                         returnArrayElement.put("imageUrl", imageUrl);
                     }
 
-
                 }
 
+                // Append the accumulator JSONOBject to the returnArray
                 returnArray.put(returnArrayElement);
 
             }
+
             return returnArray;
+
         } catch (JSONException e) {
             e.printStackTrace();
             JSONArray emptyArray = new JSONArray();
+
             return emptyArray;
 
         }
